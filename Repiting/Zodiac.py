23e4be6a8cc410f9c239fadbe7659e6192579b52
maya <orlovky-maya@mail.ru>
2022-11-29 16:05:02 +0300

    Zodiak task

---
+++ Repiting/Zodiac.py
@@ -0,0 +1,4 @@
+animals = ['Обезьяна', 'Петух', 'Собака', 'Свинья', 'Крыса', 'Бык', 'Тигр', 'Заяц', 'Дракон', 'Змея', 'Лошадь', 'Овца']
+year = int(input())
+index_of_animals = year % 12
+print(animals[index_of_animals])
