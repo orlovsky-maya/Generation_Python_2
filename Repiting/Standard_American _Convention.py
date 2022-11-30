aaf60f39e6604e4a8f5a68a155f4b05fea2078f5
maya <orlovky-maya@mail.ru>
2022-11-30 10:29:19 +0300

    number_flip task

---
+++ Repiting/Standard_American _Convention.py
@@ -0,0 +1,27 @@
+# n = int(input())
+# converted_n = []
+# counter = 0
+# while n != 0:
+#     last_digit = n % 10
+#     counter += 1
+#     converted_n.append(last_digit)
+#     if counter == 3:
+#         converted_n.append(',')
+#         counter -= 3
+#     n //= 10
+# if converted_n[-1] == ',':
+#     del converted_n[-1]
+# converted_n.reverse()
+# print(* converted_n, sep='')
+#
+
+# num = input()
+# for idx in range(len(num) - 3, 0, -3):
+#     num = num[:idx] + ',' + num[idx:]
+# print(num)
+
+
+# print(f'{int(input()):,}')
+
+a = int(input())
+print(f'{a:,}')
