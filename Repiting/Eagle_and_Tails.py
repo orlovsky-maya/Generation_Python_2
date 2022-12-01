cd3fba891f934c04872ddc1511a05684a0b39c8e
maya <orlovky-maya@mail.ru>
2022-12-01 13:30:18 +0300

    Eagle_and_Tails.py task

---
+++ Repiting/Eagle_and_Tails.py
@@ -0,0 +1,17 @@
+# s = input()
+# counter_max = 0
+# counter_tails = 0
+# for i in range(len(s)):
+#     if s[i] == 'Р':
+#         counter_tails += 1
+#     else:
+#         if counter_max < counter_tails:
+#             counter_max = counter_tails
+#         counter_tails = 0
+# if counter_max < counter_tails:
+#     counter_max = counter_tails
+# print(counter_max)
+
+
+s = input().split('О')
+print(len(max(s)))
