2847516e6a0969f7672cdcfb5b33fd5baa190b44
maya <orlovky-maya@mail.ru>
2022-12-09 14:13:58 +0300

    matrix task

---
+++  Matrices/Column_swap.py
@@ -0,0 +1,22 @@
+n = int(input())
+m = int(input())
+matrix = [[_ for _ in input().split()] for _ in range(n)]
+numbers = [int(num) for num in input().split()]
+i = numbers[0]
+j = numbers[1]
+for r in range(n):
+    matrix[r][i], matrix[r][j] = matrix[r][j], matrix[r][i]
+for r in range(n):
+    for c in range(m):
+        print(matrix[r][c], end=' ')
+    print()
+
+n, m = int(input()), int(input())
+matrix = [input().split() for _ in range(n)]
+col1, col2 = [int(i) for i in input().split()]
+
+for i in range(n):
+    matrix[i][col1], matrix[i][col2] = matrix[i][col2], matrix[i][col1]
+
+for row in matrix:
+    print(*row)
