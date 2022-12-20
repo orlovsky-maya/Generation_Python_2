1c659f1911abc80d8ca0ff90ebb8d165504f9699
maya <orlovky-maya@mail.ru>
2022-12-20 10:49:26 +0300

    matrix task

---
+++ tuples/tup_4.py
@@ -0,0 +1,29 @@
+# n = int(input())
+# list_all = []
+# list_more_3 = []
+# for i in range(n):
+#     elem = tuple(input().split())
+#     list_all.append(elem)
+#     if int(elem[1]) > 3:
+#         list_more_3.append(elem)
+# for elem in list_all:
+#     print(*elem)
+# print()
+# for elem in list_more_3:
+#     print(*elem)
+
+
+students = [tuple(input().split()) for _ in range(int(input()))]
+
+for student in students:
+    print(*student)
+
+print()
+
+for name, grade in students:
+    if int(grade) > 3:
+        print(name, grade)
+
+
+
+
