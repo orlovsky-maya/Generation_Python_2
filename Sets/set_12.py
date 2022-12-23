a4346d97188bc6c2e06ac1d3587814a5b0c6a100
maya <orlovky-maya@mail.ru>
2022-12-23 09:31:03 +0300

    sets task

---
+++ Sets/set_12.py
@@ -0,0 +1,22 @@
+numbers = [int(num) for num in input().split()]
+numbers_set = set()
+length = 0
+for c in numbers:
+    numbers_set.add(c)
+    if length != len(numbers_set):
+        print('NO')
+        length = len(numbers_set)
+    else:
+        print('YES')
+
+
+
+numbers = [int(i) for i in input().split()]
+myset = set()
+
+for i in numbers:
+    if i in myset:
+        print('YES')
+    else:
+        print('NO')
+        myset.add(i)
