1f6177c2e86d30fcde952e1cd7b4fc162ae04ed4
maya <orlovky-maya@mail.ru>
2022-12-26 10:15:04 +0300

    sets task

---
+++ Sets/set_24.py
@@ -0,0 +1,3 @@
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+myset = {words[i][0].lower() for i in range(len(words))}
+print(*sorted(myset))
