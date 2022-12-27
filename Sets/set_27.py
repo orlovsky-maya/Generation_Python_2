9aea5a5313d94424e323054ed09390ccf2203642
maya <orlovky-maya@mail.ru>
2022-12-27 08:06:15 +0300

    sets task

---
+++ Sets/set_27.py
@@ -0,0 +1,12 @@
+# files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT',
+#          'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py',
+#          'stepik.org', 'kotlin.ko', 'github.git']
+# myset = {word.lower() for word in files if word[-4:].lower() == '.png'}
+# print(*sorted(myset))
+
+
+set1 = frozenset('beegeek')
+set2 = frozenset('stepik')
+
+set3 = set1 & set2
+print(set3)
