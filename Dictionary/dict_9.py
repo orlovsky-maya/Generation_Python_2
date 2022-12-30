5826c293135ca9076d51047c2c6ef138bb6ca163
maya <orlovky-maya@mail.ru>
2022-12-30 10:47:21 +0300

    sets task

---
+++ Dictionary/dict_9.py
@@ -0,0 +1,17 @@
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+result = dict1.copy()
+
+for key in dict2:
+    result[key] = dict1.get(key, 0) + dict2.get(key, 0)
+
+print(result)
+
+
+dict1 = {'a': 100, 'z': 333, 'b': 200, 'c': 300, 'd': 45, 'e': 98, 't': 76, 'q': 34, 'f': 90, 'm': 230}
+dict2 = {'a': 300, 'b': 200, 'd': 400, 't': 777, 'c': 12, 'p': 123, 'w': 111, 'z': 666}
+
+result = dict1.copy()
+for key, value in dict2.items():
+    result[key] = result.get(key, 0) + value
