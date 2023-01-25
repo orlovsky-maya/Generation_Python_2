0ae684b23629486705deb782f02b5690c7adb946
maya <orlovky-maya@mail.ru>
2023-01-25 11:14:50 +0300

    sets task

---
+++ Sets/SET_2.py
@@ -0,0 +1,6 @@
+# first = set(input().split())
+# second = set(input().split())
+# myset = first.intersection(second)
+# print(len(myset))
+
+print(len(set(input().split()).intersection(set(input().split()))))
