0ad086339238eeb2e066d3efd341ffe18409ef0f
maya <orlovky-maya@mail.ru>
2023-01-30 14:27:15 +0300

    dict task

---
+++ Dictionary/dict_24.py
@@ -0,0 +1,4 @@
+favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
+
+result = {key: favorite_numbers[key] for key in favorite_numbers if 9 < favorite_numbers[key] < 100}
+print(result)
