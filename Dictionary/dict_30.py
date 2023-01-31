d02b1724122a0cca2761b9660d1b94b2268262fb
maya <orlovky-maya@mail.ru>
2023-01-31 12:47:40 +0300

    dict task

---
+++ Dictionary/dict_30.py
@@ -0,0 +1,4 @@
+students = {'Timur': (170, 75), 'Ruslan': (180, 105), 'Soltan': (192, 68), 'Roman': (175, 70), 'Madlen': (160, 50), 'Stefani': (165, 70), 'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67), 'Scott': (168, 78), 'John': (186, 79), 'Alex': (195, 120), 'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80), 'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80), 'Mary': (175, 69), 'Jane': (190, 80)}
+
+result = {key: students[key] for key in students if students[key][0] > 167 and students[key][1] < 75}
+print(result)
