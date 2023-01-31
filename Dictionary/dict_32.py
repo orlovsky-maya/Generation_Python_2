9f6d824ed2f624444784b849f347ca77a60bf0f3
maya <orlovky-maya@mail.ru>
2023-01-31 15:17:47 +0300

    dict task

---
+++ Dictionary/dict_32.py
@@ -0,0 +1,16 @@
+student_ids = ['S001', 'S002', 'S003', 'S004', 'S005', 'S006', 'S007', 'S008', 'S009', 'S010', 'S011', 'S012', 'S013']
+student_names = ['Camila Rodriguez', 'Juan Cruz', 'Dan Richards', 'Sam Boyle', 'Batista Cesare', 'Francesco Totti', 'Khalid Hussain', 'Ethan Hawke', 'David Bowman', 'James Milner', 'Michael Owen', 'Gary Oldman', 'Tom Hardy']
+student_grades = [86, 98, 89, 92, 45, 67, 89, 90, 100, 98, 10, 96, 93]
+
+
+
+result = [{id: info} for id, info in dict(zip(student_ids, [{key: value} for key, value in dict(zip(student_names, student_grades)).items()])).items()]
+print(result)
+
+result = [{x: {y: z}} for x, y, z in zip(student_ids, student_names, student_grades)]
+
+
+
+
+
+
