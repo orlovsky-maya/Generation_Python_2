7132fa9ec7d2da9612a0f1468449de7ee54f9a97
maya <orlovky-maya@mail.ru>
2023-01-31 09:43:16 +0300

    dict task

---
+++ Dictionary/dict_26.py
@@ -0,0 +1,12 @@
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+
+
+result = {int(key): value for key, value in [i.split(':')for i in s.split()]}
+print(result)
+
+result = {int(i.split(':')[0]): i.split(':')[1] for i in s.split()}
+print(result)
+
+
+
+
