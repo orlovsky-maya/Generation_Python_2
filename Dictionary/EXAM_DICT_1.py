f5e2cbae9a7c61300bb8ebdc493b15deceb47e04
maya <orlovky-maya@mail.ru>
2023-02-03 08:43:35 +0300

    dict task

---
+++ Dictionary/EXAM_DICT_1.py
@@ -0,0 +1,9 @@
+my_dict = {'C1': [10, 20, 30, 7, 6, 23, 90], 'C2': [20, 30, 40, 1, 2, 3, 90, 12], 'C3': [12, 34, 20, 21], 'C4': [22, 54, 209, 21, 7], 'C5': [2, 4, 29, 21, 19], 'C6': [4, 6, 7, 10, 55], 'C7': [4, 8, 12, 23, 42], 'C8': [3, 14, 15, 26, 48], 'C9': [2, 7, 18, 28, 18, 28]}
+# for value in my_dict.values():
+#     for i in value:
+#         if i >= 20:
+#             value.remove(i)
+# print(my_dict)
+
+my_dict = {key: [num for num in value if num <= 20] for key, value in my_dict.items()}
+print(my_dict)
