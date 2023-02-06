e6c09f567256273ad19268e9e29bf921fe1354b0
maya <orlovky-maya@mail.ru>
2023-02-06 10:26:26 +0300

    dict task

---
+++ Random_motod/Ran_3.py
@@ -0,0 +1,12 @@
+import random
+
+length = int(input())
+for i in range(length):
+    print(((chr(random.randint(65, 90)), chr(random.randint(97, 122)))[random.randint(0, 1)]), end='')
+
+import random
+
+length = int(input())    # длина пароля
+
+for i in range(length):
+    print(chr(random.randint(65, 90) or random.randint(97, 122)), end='')
