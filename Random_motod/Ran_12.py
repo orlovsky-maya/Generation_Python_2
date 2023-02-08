2bbfd843a4d3db76b776d6de39984eaba7890a30
maya <orlovky-maya@mail.ru>
2023-02-08 08:09:05 +0300

    dict task

---
+++ Random_motod/Ran_12.py
@@ -0,0 +1,60 @@
+import random
+import string
+
+
+def generate_password(length):
+    symbols = string.ascii_letters + string.digits
+    for s in 'lI1oO0':
+        if s in symbols:
+            symbols = symbols.replace(s, '')
+    return ''.join(random.sample(symbols, length))
+
+
+def generate_passwords(length):
+    return generate_password(length)
+
+
+n, m = int(input()), int(input())
+
+for i in range(n):
+    print(generate_passwords(m))
+
+
+import random
+import string
+
+
+def generate_password(length):
+    symbols = string.ascii_letters + string.digits
+    for s in 'lI1oO0':
+        if s in symbols:
+            symbols = symbols.replace(s, '')
+    return ''.join(random.sample(symbols, length))
+
+
+def generate_passwords(length):
+    return generate_password(length)
+
+
+n, m = int(input()), int(input())
+
+for i in range(n):
+    print(generate_passwords(m))
+
+
+from string import ascii_letters, digits
+from random import sample
+
+def generate_password(lenght):
+    return ''.join(sample(letter, lenght))
+
+
+def generate_passwords(count, lenght):
+    for _ in range(count):
+        print(generate_password(lenght))
+
+
+n, m = int(input()), int(input())
+letter = list((set(ascii_letters) | set(digits)) - set('lLiI1oO0'))
+
+generate_passwords(n, m)
