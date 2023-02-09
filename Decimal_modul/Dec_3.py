90801fa59a8a3011812f500d3e6b73713d437588
maya <orlovky-maya@mail.ru>
2023-02-09 09:17:06 +0300

    Dec task

---
+++ Decimal_modul/Dec_3.py
@@ -0,0 +1,17 @@
+from decimal import *
+num = Decimal(input())
+
+num_tuple = num.as_tuple()
+digits = num_tuple.digits
+exp = num_tuple.exponent
+
+if len(digits) > abs(exp):
+    print(min(digits) + max(digits))
+else:
+    print(max(digits))
+
+
+from decimal import *
+num = Decimal(input())
+cyphers = num.as_tuple().digits
+print(max(cyphers) + min(cyphers) * (abs(num) >= 1))
