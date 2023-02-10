096fb7aa290a67bd15ccbfba67629322a9dfa57f
maya <orlovky-maya@mail.ru>
2023-02-10 10:29:50 +0300

    Comp task

---
+++ Complex_num/Comp_2.py
@@ -0,0 +1,8 @@
+numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+largest = 0
+for i in numbers:
+    if abs(i) > abs(largest):
+        largest = i
+
+print(largest)
+print(abs(largest))
