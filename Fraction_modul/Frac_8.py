feae8b6a84eb03f5633639b82990a86a2b4c3c04
maya <orlovky-maya@mail.ru>
2023-02-10 10:19:58 +0300

    Frac task

---
+++ Fraction_modul/Frac_8.py
@@ -0,0 +1,23 @@
+from fractions import Fraction as F
+from math import gcd
+
+n = int(input())
+result = []
+z = n
+while z > 1:
+    for i in range(1, n + 1):
+        if gcd(i, z) == 1 and i < z:
+            result.append(F(i, z))
+    z -= 1
+
+print(*sorted(result), sep='\n')
+
+from fractions import Fraction
+
+numbers = set()
+
+for i in range(2, int(input()) + 1):
+    for j in range(1, i):
+        numbers.add(Fraction(j, i))
+
+print(*sorted(numbers), sep='\n')
