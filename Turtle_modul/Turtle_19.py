17bc12fd3cbe417924169843e11a0ee0aba001bc
maya <orlovky-maya@mail.ru>
2023-02-14 14:51:41 +0300

    Comp task

---
+++ Turtle_modul/Turtle_19.py
@@ -0,0 +1,62 @@
+# from turtle import *
+#
+# colours = ('green', 'BlueViolet', 'orange', 'red', 'blue', 'yellow')
+# size = 1
+# step = 2
+# for i in range(6):
+#     for j in range(6):
+#         pensize(size)
+#         pencolor(colours[j])
+#         left(45)
+#         forward(step)
+#         step += 2
+#     size += 2
+
+
+# colours = ((0, 0, 255), (255, 255, 0), (0, 134, 0), (166, 0, 128), (246, 182, 0), (234, 0, 0))
+#
+# from turtle import *
+# colours = ('blue', 'yellow', 'green', 'BlueViolet', 'orange', 'red')
+#
+# size = 1
+# step = 2
+# for i in range(8):
+#     for j in range(6):
+#         pensize(size)
+#         pencolor(colours[j])
+#         left(45)
+#         forward(step)
+#         step += 2
+#     size += 2
+
+
+# from turtle import *
+#
+# length, width = 10, 0.5
+# colors = ('blue', 'yellow', 'green', 'purple', 'orange', 'red')
+#
+# for i in range(44):
+#   pencolor(colors[i % 6])
+#   pensize(width)
+#   left(45)
+#   forward(length)
+#   length += 2.7
+#   width += 0.7
+
+from turtle import *
+colours = ('blue', 'yellow', 'green', 'BlueViolet', 'orange', 'red')
+
+size = 1
+step = 1
+pensize(size)
+pencolor(colours[step])
+forward(step)
+
+for i in range(7):
+    for j in range(6):
+        pensize(size)
+        pencolor(colours[j])
+        left(45)
+        forward(step)
+        step += 0.7
+    size += 0.7
