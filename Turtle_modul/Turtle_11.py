884e87741a1c903b4a265d3f1e93d8d216ad5b42
maya <orlovky-maya@mail.ru>
2023-02-14 07:46:32 +0300

    Comp task

---
+++ Turtle_modul/Turtle_11.py
@@ -0,0 +1,25 @@
+# import turtle
+#
+#
+# def square(side):
+#     for _ in range(4):
+#         turtle.backward(side)
+#         turtle.right(90)
+#
+#
+# s = 300
+# for _ in range(30):
+#     square(s)
+#     s -= 10
+
+import turtle
+def squares(side, n, step):
+    for _ in range(n):
+        side += step
+        for _ in range(4):
+            turtle.left(90)
+            turtle.forward(side)
+side = int(input('Введите длину стороны квадрата:\n'))
+n = int(input('Введите количество квадратов:\n'))
+step = int(input('Введите шаг:\n'))
+squares(side, n, step)
