146f2ab27a06491fd4a62570a01430f1da8e71be
maya <orlovky-maya@mail.ru>
2023-02-20 09:18:10 +0300

    Comp task

---
+++ Turtle_modul/Turtle_34.py
@@ -0,0 +1,16 @@
+# from turtle import *
+from math import *
+#
+#
+# def triangle(side):
+#     for _ in range(5):
+#        forward(side)
+#        right(360/5)
+#
+#
+# triangle(100)
+
+s = 300
+
+side = sqrt(s * 4 * tan(radians(180) / 3))
+print(side)
