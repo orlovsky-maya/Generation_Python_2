c9ecf5a41e39ec5c2ec86a2834678bbd4ca3232a
maya <orlovky-maya@mail.ru>
2023-02-20 09:57:25 +0300

    Comp task

--- Turtle_modul/Turtle_34.py
+++ Turtle_modul/Turtle_34.py
@@ -1,16 +1,44 @@
-# from turtle import *
-from math import *
-#
-#
-# def triangle(side):
-#     for _ in range(5):
-#        forward(side)
-#        right(360/5)
-#
-#
-# triangle(100)
+from turtle import *
 
-s = 300
+Screen().setup(680, 420)
+speed(0)
 
-side = sqrt(s * 4 * tan(radians(180) / 3))
-print(side)
+def square(side, colour):
+    fillcolor(colour)
+    begin_fill()
+    for _ in range(4):
+        forward(side)
+        left(90)
+    end_fill()
+
+
+penup()
+goto(-250, -250)
+pendown()
+square(500, 'white')
+
+s = 100
+x, y = -250, -250
+
+for _ in range(3):
+    for _ in range(3):
+        penup()
+        goto(x, y)
+        pendown()
+        square(s, 'black')
+        x += 200
+    x = -250
+    y += 200
+
+x, y = -150, -150
+
+for _ in range(2):
+    for _ in range(2):
+        penup()
+        goto(x, y)
+        pendown()
+        square(s, 'black')
+        x += 200
+    x = -150
+    y += 200
+done()
