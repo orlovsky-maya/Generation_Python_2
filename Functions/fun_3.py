80e81654a735f0691c1659cbda45404529e0138d
maya <orlovky-maya@mail.ru>
2023-02-22 10:28:28 +0300

    Comp task

---
+++ Functions/fun_3.py
@@ -0,0 +1,11 @@
+def sq_sum(*args):
+    summa = 0
+    for i in range(len(args)):
+        summa += args[i] ** 2
+    return summa
+
+print(sq_sum())
+print(sq_sum(2))
+print(sq_sum(1.5, 2.5))
+print(sq_sum(1, 2, 3))
+print(sq_sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
