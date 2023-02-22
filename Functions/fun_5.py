70b07f54d1e5cbf28662bc77e5697fa06dfcff31
maya <orlovky-maya@mail.ru>
2023-02-22 14:12:01 +0300

    Comp task

---
+++ Functions/fun_5.py
@@ -0,0 +1,16 @@
+def greet(name, *args):
+    if len(args) == 0:
+        return f'Hello, {name}!'
+    else:
+        return f'Hello, {name} and {" and ".join(args)}!'
+
+print(greet('Timur'))
+print(greet('Timur', 'Roman'))
+print(greet('Timur', 'Roman', 'Ruslan'))
+
+
+
+
+def greet(name, *args):
+    return f'Hello, {" and ".join((name,) + args)}!'
+
