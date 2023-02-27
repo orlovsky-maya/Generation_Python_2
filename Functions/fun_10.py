5d0a63974a07587c644b2a7c27b3abb9c8573b6d
maya <orlovky-maya@mail.ru>
2023-02-27 10:24:42 +0300

    Comp task

---
+++ Functions/fun_10.py
@@ -0,0 +1,9 @@
+def sum_min_max(point):
+    return min(point) + max(point)
+
+
+numbers = [(10, 10, 10), (30, 45, 56), (81, 80, 39), (1, 2, 3), (12, 45, 67), (-2, -4, 100), (1, 2, 99), (89, 90, 34), (10, 20, 30), (50, 40, 50), (34, 78, 65), (-5, 90, -1)]
+numbers.sort(key=sum_min_max)
+
+
+print(numbers)
