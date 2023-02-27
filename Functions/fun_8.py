9343d64a7185b5d5bfb39a9813e2a3ffd7fcc81a
maya <orlovky-maya@mail.ru>
2023-02-27 10:08:31 +0300

    Comp task

---
+++ Functions/fun_8.py
@@ -0,0 +1,7 @@
+def arithmetic_mean(point):
+    return sum(point) / len(point)
+
+
+numbers = [(10, 10, 10), (30, 45, 56), (81, 39), (1, 2, 3), (12,), (-2, -4, 100), (1, 2, 99), (89, 9, 34), (10, 20, 30, -2), (50, 40, 50), (34, 78, 65), (-5, 90, -1, -5), (1, 2, 3, 4, 5, 6), (-9, 8, 4), (90, 1, -45, -21)]
+print(min(numbers, key=arithmetic_mean))
+print(max(numbers, key=arithmetic_mean))
