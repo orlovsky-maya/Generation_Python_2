8de69f5abcd3b1eaaba2eb6c5a97901fa2acd939
maya <orlovky-maya@mail.ru>
2023-03-01 12:45:12 +0300

    Comp task

---
+++ Functions/fun_21.py
@@ -0,0 +1,30 @@
+from functools import reduce
+
+data = [['Tokyo', 35676000, 'primary'],
+        ['New York', 19354922, 'nan'],
+        ['Mexico City', 19028000, 'primary'],
+        ['Mumbai', 18978000, 'admin'],
+        ['Sao Paulo', 18845000, 'admin'],
+        ['Delhi', 15926000, 'admin'],
+        ['Shanghai', 14987000, 'admin'],
+        ['Kolkata', 14787000, 'admin'],
+        ['Los Angeles', 12815475, 'nan'],
+        ['Dhaka', 12797394, 'primary'],
+        ['Buenos Aires', 12795000, 'primary'],
+        ['Karachi', 12130000, 'admin'],
+        ['Cairo', 11893000, 'primary'],
+        ['Rio de Janeiro', 11748000, 'admin'],
+        ['Osaka', 11294000, 'admin'],
+        ['Beijing', 11106000, 'primary'],
+        ['Manila', 11100000, 'primary'],
+        ['Moscow', 10452000, 'primary'],
+        ['Istanbul', 10061000, 'admin'],
+        ['Paris', 9904000, 'primary']]
+
+filter_result = list(filter(lambda city: city[1] > 10000000 and city[2] == 'primary', data))
+map_result = list(map(lambda city: city[0], filter_result))
+map_result = sorted(map_result)
+reduce_result = reduce(lambda name_1, name_2: name_1 + ', ' + name_2, map_result, 'Cities:')
+
+
+print(reduce_result)
