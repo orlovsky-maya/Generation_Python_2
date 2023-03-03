ce17d65d9ee80959e6ccbc755367c6d4d2841357
maya <orlovky-maya@mail.ru>
2023-03-03 14:46:04 +0300

    Comp task

---
+++ Functions/fun_34.py
@@ -0,0 +1,20 @@
+# def ignore_command(command):
+#     ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+#
+#     for word in ignore:
+#         if word in command:
+#             return True
+#     return False
+#
+
+
+def ignore_command(command):
+    ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
+    result = list(map(lambda word: True if word in command else False, ignore))
+    return any(result)
+
+
+print(ignore_command('get ip'))
+print(ignore_command('select all'))
+print(ignore_command('delete'))
+print(ignore_command('trancate'))
