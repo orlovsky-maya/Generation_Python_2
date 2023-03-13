b2cdb6ec65047a3e8fe46970f74b5c8a76844969
maya <orlovky-maya@mail.ru>
2023-03-13 13:21:29 +0400

    Comp task

---
+++ Working_with_files/files_6.py
@@ -0,0 +1,12 @@
+file = open('prices.txt')
+result_summa = 0
+for line in file:
+    summa = int(line.split()[2]) * int(line.split()[1])
+    result_summa += summa
+print(result_summa)
+
+
+file = open('prices.txt')
+lines = map(str.split, file)
+print(sum(map(lambda line: int(line[1]) * int(line[2]), lines)))
+file.close()
