1cc2bcd623a1875b11717a99d76a2d069cd7ee97
maya <orlovky-maya@mail.ru>
2023-03-14 15:05:41 +0400

    Comp task

--- Working_with_files/file_10.py
+++
@@ -1,9 +0,0 @@
-with open('numbers.txt') as file:
-    file = list(map(str.split, file.readlines()))
-    for i in range(len(file)):
-        print(sum(map(int, file[i])))
-
-
-with open('numbers.txt') as f:
-    for line in f:
-        print(sum(map(int, line.split())))
---
+++ Working_with_files/files_10.py
@@ -0,0 +1,10 @@
+with open('numbers.txt') as file:
+    lls = list(map(str.split, file.readlines()))
+    for l in lls:
+        print(sum(map(int, l)))
+
+
+with open('numbers.txt') as f:
+    for line in f:
+        print(sum(map(int, line.split())))
+
