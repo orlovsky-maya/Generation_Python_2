58e035c3345a485d4d01a10dba80f5ebb397a97a
maya <orlovky-maya@mail.ru>
2023-03-16 08:30:41 +0400

    Comp task

---
+++ Working_with_files/files_17.py
@@ -0,0 +1,15 @@
+from random import *
+
+with open('random.txt', 'w', encoding='utf-8') as file:
+    for i in range(25):
+        file.write(str(randint(111, 777)))
+        file.write('\n')
+
+from random import*
+with open('random.txt', 'w', encoding='utf-8') as file:
+    for i in range(25):
+        file.write(str(randint(111,777))+'\n')
+
+from random import randrange
+with open('random.txt','w') as out:
+    print(*[randrange(111,778) for _ in range(25)],sep='\n',file=out)
