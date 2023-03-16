8d7c249c076643a1195efbe630c90aaf3db5a661
maya <orlovky-maya@mail.ru>
2023-03-16 11:12:01 +0400

    file task

--- Working_with_files/files_19.py
+++ Working_with_files/files_19.py
@@ -1,4 +1,16 @@
 with open('class_scores.txt', 'r') as old_scores, open('new_scores.txt', 'w') as new_scores:
     for line in old_scores:
         name, score = line.split()
-        new_scores.writelines(f'{name} {int(score) + 5}''\n')
+        if int(score) < 95:
+            new_scores.writelines(f'{name} {int(score) + 5}''\n')
+        else:
+            new_scores.writelines(f'{name} {100}''\n')
+
+
+with open('class_scores.txt') as class_scores, open('new_scores.txt', 'w') as new_scores:
+    for line in class_scores:
+        name, score = line.split()
+        score = int(score) + 5
+        if score > 100:
+            score = 100
+        print(name, score, file=new_scores)
