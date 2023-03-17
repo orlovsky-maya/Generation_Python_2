09e2118e5263fa9e99cf33da8ba9fb5395b505c6
maya <orlovky-maya@mail.ru>
2023-03-17 15:51:51 +0400

    Nested Lists: Part2
    Reviewed solutions:
    * list_according_to_sample_1
    * list_according_to_sample_2
    * pascal's_triangle_1
    * pascal's_triangle_2
    * packing_duplicates
    * partitioning_into_chunks
    * sublists

--- Nested_Lists/L_6.py
+++
@@ -1,6 +0,0 @@
-n, m = int(input()), int(input())
-
-my_list = [[0] * m ] * n
-my_list[0][0] = 17
-
-print(my_list)
--- Nested_Lists/Nested_lists. Part2/2. list_according_to_sample_2.py
+++ Nested_Lists/Nested_lists. Part2/2. list_according_to_sample_2.py
@@ -1,16 +1,16 @@
+n = int(input())
+
+
+elem = [list(range(1, i + 1)) for i in range(1, n + 1)]
+print(* elem, sep='\n')
+
+
 # n = int(input())
 # my_list = []
 # for i in range(1, n + 1):
 #     my_list.append(list(range(1, i + 1)))
 # print(* my_list, sep='\n')
 
-
-
-n = int(input())
-
-
-elem = [list(range(1, i + 1)) for i in range(1, n + 1)]
-print(* elem, sep='\n')
 
 
 
