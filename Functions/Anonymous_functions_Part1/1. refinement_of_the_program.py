24fcab4f07339c33141a67979d83a01d5a362944
maya <orlovky-maya@mail.ru>
2023-03-23 12:24:55 +0400

    Functions: Anonymous functions Part1
    Review solutions:
     * refinement_of_the_program
     * list_of_primary_cities_in_alphab_order

--- Functions/Anonymous_functions_Part1/1. refinement_of_the_program.py
+++ Functions/Anonymous_functions_Part1/1. refinement_of_the_program.py
@@ -4,7 +4,7 @@
 # words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
 # numbers = [4, 6, 9, 23, 5]
 #
-# # Исправьте этот код
+# # Исправьте этот код / Fix this code
 # map_result = list(map(lambda num: num, floats))
 # filter_result = list(filter(lambda name: name, words))
 # reduce_result = reduce(lambda num1, num2: num1 + num2, numbers, 0)
@@ -19,7 +19,7 @@ floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59, 34.23, 12.12, 4.67, 2.45, 9.
 words = ['racecar', 'akinremi', 'deed', 'temidayo', 'omoseun', 'civic', 'TATTARRATTAT', 'malayalam', 'nun']
 numbers = [4, 6, 9, 23, 5]
 
-# Исправьте этот код
+
 map_result = list(map(lambda num: round(num ** 2, 1), floats))
 filter_result = list(filter(lambda name: name == name[::-1] and len(name) > 4, words))
 reduce_result = reduce(lambda num1, num2: num1 * num2, numbers, 1)
